60ab472f45ee56bd487cfea63e8719b37007f8fd
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2025-02-17 14:09:35 +1100

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className=''>
+        <h1 className=' h-[50px] my-5 text-grey-900 font-medium text-4xl text-center '>
+          Library Management System
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
